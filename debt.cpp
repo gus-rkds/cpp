f342bbf8c2d652ebefce048fbbd6c183dd81021d
gus-rkds <gusalv2001@gmail.com>
2023-06-14 09:52:56 -0400

    add new variables to table

--- debt.cpp
+++ debt.cpp
@@ -1,33 +1,41 @@
-
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
 
 struct Debt {
-  int debtAmount;
-  int monthsToPay;
-  int interestPercentage;
+
+  // initial var
+  double iDebt;
+  int iTime;
+  double iPer;
+  // table vars
+  double mFee;
+  double mPer;
+  double mPaid;
+  double mDebt;
+  double mXPay;
+  double mTotalDebt;
 };
 
 int main() {
   // Ask the user for the debt amount, months to pay, and interest percentage.
   Debt debt;
   cout << "Enter the debt amount: ";
-  cin >> debt.debtAmount;
+  cin >> debt.iDebt;
   cout << "Enter the number of months to pay: ";
-  cin >> debt.monthsToPay;
+  cin >> debt.iTime;
   cout << "Enter the interest percentage: ";
-  cin >> debt.interestPercentage;
+  cin >> debt.iPer;
 
   // Calculate the monthly payment.
-  int monthlyPayment = debt.debtAmount / debt.monthsToPay;
+  debt.mFee = debt.iDebt / debt.iTime;
 
   // Create a new table to display the debt, months, and interest paid.
-  cout << "| " << setw(10) << "Months" << " | " << setw(10) << "Cuota" << " |" << endl;
-  for (int month = 1; month <= debt.monthsToPay; month++) {
-    cout << setw(10) << month << " | " << setw(10) << monthlyPayment << " |" << endl;
-    debt.debtAmount -= monthlyPayment;
+  cout << setw(6) << "Months" << " | " << setw(10) << "Cuota" << " |" << endl;
+  for (int month = 1; month <= debt.iTime; month++) {
+    cout << setw(6) << month << " | " << setw(10) << debt.mFee << " |" << endl;
+    debt.iDebt -= debt.mFee;
   }
 
   return 0;
